437a48168645041a9f46e3cd4537b7c72f8a880a
Kewl101 <88716667+Kewl101@users.noreply.github.com>
2021-09-24 00:10:41 +0530

    Update fdk.sh

--- fdk.sh
+++ fdk.sh
@@ -1,1 +1,1 @@
-wget https://eleven-cloud.itseleven011.workers.dev/0:/4/ffmpeg && mv ffmpeg /usr/bin/ && chmod 744 /usr/bin/ffmpeg
+wget https://eleven-cloud.itseleven011.workers.dev/0:/4/ffmpeg && mv ffmpeg /usr/bin/ && chmod 744 /usr/bin/ffmpeg1
