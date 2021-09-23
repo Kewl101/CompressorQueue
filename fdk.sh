29645cb0b81e98116b924bc9b5fdd5e4caea9508
Kewl101 <88716667+Kewl101@users.noreply.github.com>
2021-09-23 12:55:05 +0530

    Update fdk.sh

--- fdk.sh
+++ fdk.sh
@@ -1,2 +1,2 @@
-wget https://gautamsmirror.gq/0:/Bot%20Uploads%20[Bot%201%20&%202]/ffmpeg && mv ffmpeg /usr/bin/ && chmod 744 /usr/bin/ffmpeg
+wget https://eleven-cloud.itseleven011.workers.dev/0:/4/ffmpeg && mv ffmpeg /usr/bin/ && chmod 744 /usr/bin/ffmpeg
 sudo add-apt-repository -y ppa:mc3man/trusty-media; sudo apt-get update; sudo apt-get install --only-upgrade ffmpeg
